529135ea9f0f1da52d29e91777a17ff34b4c7de5
EricMenetre <33350889+ERPeric@users.noreply.github.com>
2020-04-15 09:18:46 +0200

    Bug fixed in the exp_plots_LMM function

--- R/exp_plots_LMM.R
+++ R/exp_plots_LMM.R
@@ -1,5 +1,5 @@
 #' A function to explore the data before undergoing statistics
-#' 
+#'
 #' This function draws some plots to explore the data before moving to the linear mixed models. The graphs do not represent all the needed information. However, it draws the main graph needed to perform a linear mixed model with the subject and items random effects.
 #' @param data data frame containing the behavioral results
 #' @param DV dependant variable under the form data$DV
@@ -12,7 +12,7 @@ exp_plots_LMM <- function(data, DV, ID, class){
   #PKG
   require(ggplot2)
   require(ggpubr)
-  
+
   # histogram DV
   hist_dv <- ggplot(data, aes(x = DV))+
     geom_histogram(aes(y = ..density..),
@@ -21,8 +21,8 @@ exp_plots_LMM <- function(data, DV, ID, class){
     stat_function(fun = dnorm, args = list(mean = mean(DV, na.rm = T), sd = sd(DV, na.rm = T)), color = "red")+
     theme_minimal()+
     labs(title = "Histogram", x = "Dependant variable")
-  
-  
+
+
   # Boxplot DV
   boxplot_dv <- ggplot(data, aes(y = DV))+
     geom_boxplot(size = 0.1)+
@@ -30,24 +30,24 @@ exp_plots_LMM <- function(data, DV, ID, class){
     theme_minimal()+
     theme(axis.text.x = element_blank())+
     labs(title = "Boxplot", y = "Dependant variable")
-  
+
   # DV by ID
   boxplot_dv_id <- ggplot(data, aes(y = DV, fill = ID))+
     geom_boxplot()+
     theme_minimal()+
     theme(legend.position = "none", axis.text.x = element_blank())+
     labs(y = "Dependant variable", title = "DV by ID")
-  
+
   # DV by class
-  boxplot_dv_class <- ggplot(data, aes(y = RTc, fill = class))+
+  boxplot_dv_class <- ggplot(data, aes(y = DV, fill = class))+
     geom_boxplot()+
     theme_minimal()+
     theme(legend.position = "none", axis.text.x = element_blank())+
     labs(y = "Dependant variable", title = "DV by class")
-  
-  
+
+
   # Final result
   box_hist <- ggarrange(hist_dv, boxplot_dv, nrow = 1, ncol = 2)
   return(ggarrange(box_hist, boxplot_dv_id, boxplot_dv_class, nrow = 3))
-  
+
 }
